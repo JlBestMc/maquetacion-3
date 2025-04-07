58e7f1dacda29ddb980bc5de11d935d02165499f
JlBestMc <jlrcmt@gmail.com>
2025-04-07 04:47:21 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content:["./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1d2440",
+                secondary: {
+                    100: "#5465df",
+                    200: "#ff5252",
+                }
+
+            }
+
+        }
+    }
+}
